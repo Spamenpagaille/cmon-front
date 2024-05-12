ffc2f771bf29ac17815fcb175d73e761bff5acae
Spamenpagaille <spamenpagaille@gmail.com>
2024-05-12 05:11:15 +0200

    Correction d'anomalie

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FormAuthentificationComponent } from './form-authentification/form-authentification.component';
 
 @Component({
   selector: 'app-root',
